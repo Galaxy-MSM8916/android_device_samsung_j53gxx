aa99f0b5613427bc03171570831b511732823f97
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-18 18:37:21 +0200

    system_prop: Remove sim properties
     * These are set by libinit_msm8916
    
    Change-Id: I13d1559ac989b9fc0a076d852fee599c1bca30ab

--- system_prop.mk
+++ system_prop.mk
@@ -6,11 +6,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false \
 	ro.config.zram=false
-
-# RIL
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	ro.multisim.simslotcount=2 \
-	ro.telephony.ril.config=simactivation \
-	telephony.lteOnCdmaDevice=0 \
-	persist.radio.multisim.config=dsds \
-	rild.libpath2=/system/lib/libsec-ril-dsds.so
