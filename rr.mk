0f07e53881b63f7259e7e678df4c9b7a4113b42a
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-02-18 01:23:56 -0500

    j53gxx: Add RR Makefile
    Change-Id: Ia1b01b6c228df0a75986ade5d9f0ff33699dae53

---
+++ rr.mk
@@ -0,0 +1,15 @@
+
+# Inherit from common
+$(call inherit-product, device/samsung/j5-common/rr.mk)
+
+$(call inherit-product, device/samsung/j53gxx/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j53gxx
+PRODUCT_NAME := rr_j53gxx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J500H
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
