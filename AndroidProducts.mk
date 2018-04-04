a723503feb28730938681cb2d2b71ddef97a6837
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    j53gxx: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j53gxx.mk \
 	$(LOCAL_DIR)/pa_j53gxx.mk \
 	$(LOCAL_DIR)/lineage_j53gxx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j53gxx-eng \
+    lineage_j53gxx-userdebug \
+	lineage_j53gxx-user
